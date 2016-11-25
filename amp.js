d9487969bbe05d6282243f148b4ed978d4c3f97e
Germy Laod <inyu.download@gmail.com>
2016-11-25 13:17:19 +0700

    Create amp.js

---
+++ amp.js
@@ -0,0 +1,24 @@
+<script type='text/javascript'>
+//<![CDATA[
+//Pre Auto Selection
+$('i[rel="pre"]').replaceWith(function() {
+    return $('<pre><code>' + $(this).html() + '</code></pre>');
+});
+var pres = document.querySelectorAll('pre,kbd,blockquote');
+for (var i = 0; i < pres.length; i++) {
+  pres[i].addEventListener("dblclick", function () {
+    var selection = getSelection();
+    var range = document.createRange();
+    range.selectNodeContents(this);
+    selection.removeAllRanges();
+    selection.addRange(range);
+  }, false);
+}
+//]]>
+</script>
+<script  type='text/javascript'>
+//<![CDATA[
+// Youtube Responsive
+setTimeout(function(){$(".video-youtube").each(function(){$(this).replaceWith('<iframe class="video-youtube loader" src="'+$(this).data("src")+'" allowfullscreen="allowfullscreen" height="281" width="500"></iframe>')})},5e3);
+//]]>
+</script> 
